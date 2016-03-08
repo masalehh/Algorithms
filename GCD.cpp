9434bdffdbcb496c5857d1736f5c642bc78101de
masalehh <ma.saleh007@gmail.com>
2016-03-08 12:52:21 +0600

    find gcd between two numbers a simple efficient program

---
+++ GCD.cpp
@@ -0,0 +1,41 @@
+/**
+    GCD.cpp
+    Purpose: practice data structure and algorithm implimentation
+
+    @author MA Saleh
+*/
+
+#include<iostream>
+using namespace std;
+
+int gcd(int num1, int num2)                         //to find the gcd of two numbers
+
+
+int main()                                          //main function started
+{
+    int n1, n2;
+
+    while(cin >> n1 >> n2)                          //taking input till EOF
+    cout << gcd(n1, n2) << endl;                    //call the gcd function and print it
+
+    return 0;
+}                                                   //end of main
+
+
+
+int gcd(int num1, int num2)                         //gcd function started
+{
+    int temp;
+    if(num1 % num2 == 0) return b;                  //chack if divide numbers by any one then that number will be the GCD
+    else if(num2 % num1 == 0) return a;
+
+    if( num1 >= num2) temp = num2/2;                //keep the half of samll number in temp which reduce the loop
+    else temp = num1/2;
+
+    while(temp >= 1)                                //iterate the while loop til 1 and check
+    {
+        if(num1%temp == 0 && num2%temp == 0) return temp;
+        temp--;
+
+    }
+}                                                   //end of gcd function
